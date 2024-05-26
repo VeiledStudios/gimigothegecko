b5cca9f8c60f6607729da1df80b14492407a6884
VeiledDev <stephentanevski7@gmail.com>
2024-05-26 15:35:54 +1000

    Initial Commit

---
+++ GimigoTheGecko/GimigoTheGeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GimigoTheGeckoApp.swift
+//  GimigoTheGecko
+//
+//  Created by Stephen Tanevski on 26/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct GimigoTheGeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
